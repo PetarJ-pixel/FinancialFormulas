0c95af781ad78ae51bf93bb52a11d939f1934b70
Petar Jaredic <jaredic.petar@gmail.com>
2020-06-08 17:24:35 +0200

    FinancialFormulas

---
+++ FinancialFormulas/RateOfInflation.cs
@@ -0,0 +1,13 @@
+﻿namespace FinancialFormulas
+{
+    public class RateOfInflation
+    {
+        public decimal InitialConsumerPriceIndex { get; set; }
+        public decimal EndingConsumerPriceIndex { get; set; }
+
+        public decimal CalculateRateOfInflation()
+        {
+            return (EndingConsumerPriceIndex - InitialConsumerPriceIndex) / InitialConsumerPriceIndex;
+        }
+    }
+}
---
+++ FinancialFormulas/RealRateOfReturn.cs
@@ -0,0 +1,13 @@
+﻿namespace FinancialFormulas
+{
+    public class RealRateOfReturn
+    {
+        public decimal NominalRate { get; set; }
+        public decimal InflationRate { get; set; }
+
+        public decimal CalculateRealRateOfReturn()
+        {
+            return ((1 + NominalRate) / (1 + InflationRate) - 1);
+        }
+    }
+}
